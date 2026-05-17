2b97f510768b40fead0ecd5858f42ee71cdb57c9
masadawan99 <m.asad.awan077@gmail.com>
2026-05-17 21:12:35 +0500

    fixed

--- js/stateMachine.js
+++ js/stateMachine.js
@@ -68,6 +68,6 @@ export class StateMachine {
 
     end() {
         console.log('[END] Redirect');
-        window.location.href = 'login.html';
+        window.location.href = 'signup.html';
     }
 }
